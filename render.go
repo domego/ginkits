a72a3835cadf7b655144dc9622902d8f293853d5
Kevin Ma <mlongbo@gmail.com>
2017-06-26 18:53:58 +0800

    增加错误码

--- render.go
+++ render.go
@@ -2,6 +2,24 @@ package kits
 
 import "github.com/gin-gonic/gin"
 
+const (
+	// ErrorCodeArgumentLack 缺少参数
+	ErrorCodeArgumentLack = 100
+	// ErrorCodeArgumentTypeInvalid 参数类型错误
+	ErrorCodeArgumentTypeInvalid = 101
+	// ErrorCodeInvalidSignature 签名错误
+	ErrorCodeInvalidSignature = 102
+	// ErrorCodeExpiredRequest 请求已过期
+	ErrorCodeExpiredRequest = 103
+)
+
+type RespErrorMessage struct {
+	Code    int    `json:"code"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	Message string `json:"message"`
+}
+
 // RenderSuccess 响应成功的数据
 func RenderSuccess(c *gin.Context, data interface{}) {
 	c.JSON(200, map[string]interface{}{
