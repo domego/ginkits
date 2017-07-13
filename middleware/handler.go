7364c2cd515607253d1fe6513044fbb35dd8606d
Kevin Ma <mlongbo@gmail.com>
2017-07-13 23:26:39 +0800

    增加跨域处理

--- middleware/handler.go
+++ middleware/handler.go
@@ -2,9 +2,15 @@ package middleware
 
 import "github.com/gin-gonic/gin"
 
+var (
+	AccessControlAllowOrigin = "*"
+)
+
 func CommonHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		if len(AccessControlAllowOrigin) > 0 {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", AccessControlAllowOrigin)
+		}
 		c.Next()
 	}
 }
